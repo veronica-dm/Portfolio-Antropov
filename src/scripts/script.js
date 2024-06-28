4bf5303b0de10233a272fda0816c73778bb3a1cc
Veronica <vero.dm@yandex.ru>
2024-06-28 19:36:05 +0300

    adaptive ver 1

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,12 @@
 
+function openModalMenu() {
+    document.getElementById("modal").style.top = "0px";
+}
+
+function closeModalMenu() {
+    document.getElementById("modal").style.top = "-400px";
+}
+
 function openModal() {
     document.getElementById("modal").style.top = "0px";
 }
